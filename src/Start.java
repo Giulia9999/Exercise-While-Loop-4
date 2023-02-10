2729ef6e93358b752e2ad018576f5e5438f2d8a3
Giulia <giuliac35@gmail.com>
2023-02-10 11:36:50 +0100

    Create program to guess random generated number

--- src/Start.java
+++ src/Start.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Start {
     public static void main(String[] args){
+        System.out.println("Welcome!");
+        int min = 1;
+        int max = 10;
+        int number = (int) Math.floor(Math.random() *(max - min + 1) + min);
+        System.out.print("Guess the number between 1 and 10: ");
+        Scanner scanner = new Scanner(System.in);
+        int guessNumber = scanner.nextInt();
 
+        while(guessNumber != number){
+            System.out.print("Guess the number between 1 and 10: ");
+            guessNumber = scanner.nextInt();
+        }
+        System.out.println("Congratulations!");
     }
 }
